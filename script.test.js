2ee5044f56ecf6929b4822d61660252973fea1b7
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-05-05 17:31:31 -0600

    second commit
    Co-authored-by: Valentin <valeru.251@gmail.com>
    Co-authored-by: Francisco <fran.senes@gmail.com>
    Co-authored-by: Roberto <robermancera@gmail.com>

--- script.test.js
+++ script.test.js
@@ -1,26 +1,17 @@
+var Matrix = require("./script.js");
+let matrix = new Matrix(5, 5);
 
-
-
-const Matrix = require('./script')
-//test('insertar metodo o funcion',()=>{
-//     expect().toBe();})
-//                      
-//test('insertar metodo o funcion',()=>{
-//     expect().toBe();})
-//    
-//test('Compribar matriz cuadrada',()=>{
-//     expect().toBe();})
- 
 test('Matriz no puede devolver undefined',()=>{
-   expect(getMatrix()).toBeDefined()
+    expect(matrix.getMatrix()).toBeDefined()
 })
+
 //  
 test('Dimension de Altura > 3',()=>{
-    expect(Matrix.getAltura()).toBeGreaterThan(3);
+    expect(matrix.getAltura()).toBeGreaterThan(3);
 })
 //expect(ouncesPerCan()).toBeGreaterThan(10);
 test('Dimension de Anchura > 3',()=>{
-    expect(Matrix.getAnchura() ).toBeGreaterThan(3);
+    expect(matrix.getAnchura() ).toBeGreaterThan(3);
 })
 /*
     
